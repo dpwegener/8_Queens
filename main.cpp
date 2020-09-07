22678b128bca40c289ff37c397ca6c7671b9d822
David wegener <dpwegener@gmail.com>
2020-09-07 12:35:15 -0500

    Add N Queens template
    Use bitsets to hold the board state.  The NQueens struct can support an
    arbitrary number of queens.  However, the compute power to complete the
    analysis will continue to grow as N increases.

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include "eight_queens.hpp"
+#include "n_queens.hpp"
 #include <iostream>
 
 int main(int, char**) {
@@ -7,5 +8,35 @@ int main(int, char**) {
     unsigned char board[8]{0,0,0,0,0,0,0,0};
     addQueen(board, 0);
     
-    std::cout << "Found all boards\n";
+    std::cout << "Found all 8 queen boards\n";
+
+    std::cout << "Finding all valid 9 queen boards\n\n";
+    N_Queens<9> queens9;
+    queens9.addQueen(0);
+    
+    std::cout << "Found all 9 queen boards\n";
+
+    std::cout << "Finding all valid 10 queen boards\n\n";
+    N_Queens<10> nqueens;
+    nqueens.addQueen(0);
+    
+    std::cout << "Found all 10 queen boards\n";
+
+    std::cout << "Finding all valid 11 queen boards\n\n";
+    N_Queens<11> queens11;
+    queens11.addQueen(0);
+    
+    std::cout << "Found all 11 queen boards\n";
+
+    std::cout << "Finding all valid 12 queen boards\n\n";
+    N_Queens<12> queens12;
+    queens12.addQueen(0);
+    
+    std::cout << "Found " << queens12.validBoardCount << " 12 queen boards\n";
+
+    std::cout << "Finding all valid 13 queen boards\n\n";
+    N_Queens<13> queens13;
+    queens13.addQueen(0);
+    
+    std::cout << "Found " << queens13.validBoardCount << " 13 queen boards\n";
 }
